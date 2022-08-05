b6df62897e2a4db91e344a4f977e7b63f12e6dc9
haoleng-Wick <yhx2604918320@163.com>
2022-08-05 20:54:20 +0800

    非递增递归的最小子序列

---
+++ MinSubSequence.c
@@ -0,0 +1,70 @@
+/*
+ *
+ * 非递增顺序的最小子序列
+ * 给你一个数组,请从中返回一个子序列
+ * 满足其元素之和严格大于未包含在该子序列的各元素之和
+ * 示例 nums = [4, 3, 10, 9, 8]
+ * out: [10, 9]
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b)
+{
+	return *(int *)a - *(int *)b;
+}
+
+int main(int argc, char **argv)
+{
+	int numsSize = argc - 1;
+	int nums[numsSize];
+	int *returnSize;
+
+	for(int i = 0; i < numsSize; ++i)
+	{
+		sscanf(argv[i+1], "%d", &nums[i]);
+	}
+
+	int sum = 0;
+	for (int i = 0; i < numsSize; ++i)
+	{
+		sum += nums[i];
+	}
+
+/* C语言自带的排序函数 
+ * 与上面的cmp配合使用 */
+	qsort(nums, numsSize, sizeof(int), cmp);
+	printf("qsort nums = [ ");
+	for (int i = 0; i < numsSize; ++i)
+	{
+		printf("%d ", nums[i]);
+	}
+	printf("]\n");
+
+/* 将顺序数组从后向前加,直至满足条件并返回*/
+	int *out_nums = (int *)malloc(sizeof(int) * numsSize);
+	int cur =0, pos = 0;
+	for(int i = numsSize - 1; i >= 0; --i)
+	{
+		cur += nums[i];
+		out_nums[pos++] = nums[i];
+		if (sum - cur < cur)
+			break;
+	}
+
+	returnSize = &pos;
+	printf("returnSize = %d\n", *returnSize);
+
+	printf("out_nums = [ ");
+	for (int i = 0; i < *returnSize; ++i)
+	{
+		printf("%d ", out_nums[i]);
+	}
+	printf("]\n");
+
+	free(out_nums);
+
+	return 0;
+}
