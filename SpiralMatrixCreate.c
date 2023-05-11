00c7796c03e7d8a0dfabbaecd06f5bbb0f5a75e9
Hao_Leng <64140384+haoleng-Wick@users.noreply.github.com>
2023-05-11 16:42:16 +0800

    螺旋创建矩阵

--- SpiralMatrixCreate.c
+++ SpiralMatrixCreate.c
@@ -1,9 +1,9 @@
 /*
  * LeetCode 59
- * 给一个正整数n，生成一个包含1~n^2所有元素
- * 且按照顺时针螺旋排列的n x n方阵matrix
+ * 给正整数n,m，生成一个包含1~nxm所有元素
+ * 且按照顺时针螺旋排列的n x m矩阵matrix
  *
- * intput: n = 3;
+ * intput: n = 3, m = 3;
  * output:
  *          1  2  3
  *          8  9  4
@@ -14,23 +14,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 传入矩阵的行数，传出矩阵的行数，传出矩阵每行的列数
-int **generate_matrix(int n, int *return_size, int **return_col_sizes)
+// 传入矩阵的行、列数，传出矩阵的行数，传出矩阵每行的列数
+int **generate_matrix(int n, int m, int *return_size, int **return_col_sizes)
 {
     int count = 0;
-    int **matrix = (int **)malloc(n * sizeof(int *));
+    int **matrix = (int **)malloc(n * sizeof(int *));   // row
     *return_size = n;
     *return_col_sizes = (int *)malloc(n * sizeof(int));
 
     for(int i = 0; i < n; i++)
     {
-        matrix[i] = (int *)malloc(n * sizeof(int)); // 为每行分配每列的内存
+        matrix[i] = (int *)malloc(m * sizeof(int)); // 为每行分配每列的内存
         // 复制字符0到matrix[i]所指向字符串的前n个位置(即该行的每列)
-        memset(matrix[i], 0, n * sizeof(int));
-        (*return_col_sizes)[i] = n; // 返回该行所含的列数
+        memset(matrix[i], 0, m * sizeof(int));
+        (*return_col_sizes)[i] = m; // 返回该行所含的列数
     }
 
-    int left =0, right = n - 1, top = 0, bottom = n - 1;
+    int left = 0, right = m - 1, top = 0, bottom = n - 1;
 
     while(left <= right && top <= bottom)
     {
@@ -56,18 +56,19 @@ int **generate_matrix(int n, int *return_size, int **return_col_sizes)
 
 int main(int argc, char **argv)
 {
-    if(argc != 2)
-        fprintf(stderr, "Usage: ./a.out 3\n");
+    if(argc != 3)
+        fprintf(stderr, "Usage: ./a.out 3 4\n");
 
-    int n = atoi(argv[1]);
+    int n = atoi(argv[1]), m = atoi(argv[2]);
+
     int count = 0;
     int *col_sizes;
 
-    int **matrix = generate_matrix(n, &count, &col_sizes);
+    int **matrix = generate_matrix(n, m, &count, &col_sizes);
 
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < m; j++)
         {
             printf("%3d ", matrix[i][j]);
         }
